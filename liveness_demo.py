314714edfd06111de797486ee9ae28a60006a7d0
CharalambosIoannou <cioannou1997@gmail.com>
2019-11-16 10:49:39 +0000

    Push whole project

---
+++ liveness_demo.py
@@ -0,0 +1,84 @@
+import pickle
+import time
+
+import cv2
+import imutils
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+
+"""
+Domain shifting problem- using a different dataset
+look for domain adaptation algorithm
+
+
+"""
+
+# wears_glasses = input("Do you wear glasses? Type y or n \n")
+wears_glasses = 'n'
+
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+if (wears_glasses == "n") :
+	print("Loading no_glasses_model")
+	model = load_model('no_glasses_model_with_me.h5')
+	le = pickle.loads(open('no_glasses_pickle_with_me.pickle', "rb").read())
+else :
+	print("Loading glasses_model")
+	model = load_model('dataset/Glasses/glasses_model.h5')
+	le = pickle.loads(open('dataset/Glasses/glasses_pickle.pickle', "rb").read())
+
+
+print("Starting camera")
+vs = cv2.VideoCapture(0)
+time.sleep(2.0)
+while True :
+
+	ret, frame = vs.read()
+	frame = imutils.resize(frame, width=600)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# detect faces in the grayscale frame
+	faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.3,
+			minNeighbors=3,
+			minSize=(30, 30)
+	)
+	for (x, y, w, h) in faces :
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		#get pixel locations of the box to extract face
+		roi_color = frame[y :y + h, x :x + w]
+		face = frame[y :y + h, x :x + w]
+		cv2.imshow("Frame1", face)
+		face = cv2.resize(face, (32, 32))
+		cv2.imshow("Frame2", face)
+		face = face.astype("float") / 255.0
+		face = img_to_array(face)
+		face = np.expand_dims(face, axis=0)
+
+
+		preds = model.predict(face)[0]
+		j = np.argmax(preds)
+		label = le.classes_[j]
+
+		# draw the label and bounding box on the frame
+		label = "{}: {:.4f}".format(label, preds[j])
+		cv2.putText(frame, label, (x, y - 10),
+		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+		cv2.rectangle(frame, (x, y), (x + w, y + h),
+		              (0, 0, 0), 2)
+
+	# show the output frame and wait for a key press
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q") :
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
---
+++ train_liveness.py
@@ -0,0 +1,158 @@
+# USAGE
+# python train_liveness.py --dataset dataset --model liveness.model --le le.pickle
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+
+
+
+matplotlib.use("Agg")
+
+from CNN.livenessnet import LivenessNet
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import cv2
+from keras.optimizers import Adam
+import time
+from keras.callbacks import TensorBoard
+from sklearn.metrics import classification_report
+import glob
+
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+
+# initialize the initial learning rate, batch size, and number of
+# epochs to train for
+INIT_LR = 1e-4
+BS = 10
+EPOCHS = 20
+# Define the Keras TensorBoard callback.
+NAME = "Live vs Fake photos" + str(int(time.time()))
+tensorboard_callback = TensorBoard(log_dir="logs\\{}".format(NAME))
+
+# grab the list of images in our dataset directory, then initialize
+# the list of data (i.e., images) and class images
+print("[INFO] loading images...")
+data = []
+labels = []
+labels1 = ["ImposterFace", "ClientFace"]
+for label_name in labels1:
+	print('Doing label: ' , label_name)
+	for imagePath in glob.iglob(f'dataset/Detectedface/{label_name}/*/*.jpg'):
+		print(imagePath)
+		# extract the class label from the filename, load the image and
+		# resize it to be a fixed 32x32 pixels, ignoring aspect ratio
+		image = cv2.imread(imagePath)
+		
+		# print(imagePath)
+		image = cv2.resize(image, (32, 32))
+		
+		# update the data and labels lists, respectively
+		data.append(image)
+		if (label_name == 'ImposterFace'):
+			labels.append(0)
+		else:
+			labels.append(1)
+
+print(len(data))
+# convert the data into a NumPy array, then preprocess it by scaling
+# all pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+
+# encode the labels (which are currently strings) as integers and then
+# one-hot encode them
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+labels = np_utils.to_categorical(labels, 2)
+
+
+
+# training data ( trainX ) and training labels ( trainY ).
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+												  test_size=0.20, random_state=42)
+
+
+	
+# apply data augmentation, randomly translating, rotating, resizing, etc. images on the fly.
+# enabling our model to generalize better
+aug = ImageDataGenerator(rotation_range=20, zoom_range=0.15,
+						 width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
+						 horizontal_flip=True, fill_mode="nearest")
+
+# initialize the optimizer and model
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model = LivenessNet.build(width=32, height=32, depth=3,
+						  classes=len(le.classes_))
+
+np.savetxt('feature_extraction/features.csv', model.predict(trainX, batch_size=BS), delimiter=',')
+np.savetxt('feature_extraction/labels.csv', trainY, delimiter=',')
+
+np.savetxt('feature_extraction/features.txt', model.predict(trainX, batch_size=BS), delimiter=',')
+np.savetxt('feature_extraction/labels.txt', trainY, delimiter=',')
+
+model.add(Activation("relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# softmax classifier
+model.add(Dense(len(le.classes_)))
+model.add(Activation("softmax"))
+
+
+
+model.compile(loss="binary_crossentropy", optimizer=opt,
+			  metrics=["accuracy"])
+
+
+
+# train the network
+print("[INFO] training network for {} epochs...".format(EPOCHS))
+# fit generator is on infinite look so do steps per epoch to terminate it
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+						validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+						epochs=EPOCHS, callbacks=[tensorboard_callback])
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=BS) #TODO rerun file and find mean value and visualize features on a histogram + tsne
+print(classification_report(testY.argmax(axis=1),
+							predictions.argmax(axis=1), target_names=le.classes_))
+
+# save the network to disk
+print("[INFO] serializing network to '{}'...".format('glasses_model.h5'))
+# model.save('liveness.model')
+model.save('no_glasses_model_with_me.h5')
+
+# save the label encoder to disk
+f = open('no_glasses_pickle_with_me.pickle', "wb")
+f.write(pickle.dumps(le))
+f.close()
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, EPOCHS), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, EPOCHS), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig('plot.png')
+
+score = model.evaluate_generator(aug.flow(trainX, trainY, batch_size=BS), verbose=1, steps=500)
+print("Metric Names are : ", model.metrics_names)  # ['loss', 'accuracy']
+print("Final Accuracy is: " + str(score))
+print("Shape trainX", trainX.shape)
+print("Shape testX", testX.shape)
+print("Shape trainY", trainY.shape)
+print("Shape testY", testY.shape)
